bb96538b90d245e327ed1568c5ab5eb341fae101
uzayr121 <uzayr121.uk@gmail.com>
2025-02-26 15:20:54 +0000

    docker challenge

--- r-f/app.py
+++ r-f/app.py
@@ -1,22 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, jsonify
 import redis # type: ignore
 
 app = Flask(__name__)
 
 redis_host = os.getenv ('REDIS_HOST', 'redis')
 redis_port = int(os.getenv ('REDIS_PORT', '6379'))
-r= redis.Redis(host=redis_host, port=redis_port)
+r = redis.Redis(host=redis_host, port=redis_port)
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/count')
 def count():
-    visits=r.incr('hits')
-    return "Number of hits: {}".format(visits)
+    visits = r.incr('hits')
+    return jsonify({'count': visits})
 
-#return 'Hello World!'
+@app.route('/count-page')
+def count_page():
+    return render_template('count.html')
+                
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
